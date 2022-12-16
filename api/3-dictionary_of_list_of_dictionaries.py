ce4654ccff40d9168811cb97b4183d4fd0928144
jesus <jesusmanuel.maciasmartinez@gmail.com>
2022-12-15 23:05:54 -0500

    Dictionary of list of dictionaries

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -6,22 +6,22 @@ import requests
 from sys import argv
 
 if __name__ == "__main__":
-    all_users = requests.get("https://jsonplaceholder.typicode.com/users")
-    users_dict = {}
+    todos = requests.get("https://jsonplaceholder.typicode.com/todos")
+    users_url = "https://jsonplaceholder.typicode.com/users?id="
+    from collections import defaultdict
+    users_dict = defaultdict(list)
 
-    for user in all_users.json():
-        uid = user['id']
-        user_name = user["name"]
-        todos = requests.get("https://jsonplaceholder.typicode.com/users/{}/todos"
-                             .format(uid))
-        task_list = []
+    for item in todos.json():
+        uid = item['id']
+        url = users_url + str(item['userId'])
+        usr_resp = requests.get(url).json()
+        user_name = usr_resp[0]["username"]
         
-        for task in todos.json():
-            task_list.append({"username": user_name,
-                              "task": task["title"],
-                              "completed": task["completed"]})
-        
-        users_dict["{}".format(uid)] = task_list
+        all_item = {"username": user_name,
+                     "task": item["title"],
+                     "completed": item["completed"]}
+
+        users_dict["{}".format(uid)].append(all_item)
 
     with open("todo_all_employees.json".format(uid), 'w') as f:
         json.dump(users_dict, f)
