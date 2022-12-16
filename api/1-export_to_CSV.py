45189ca922a5a576ec1dc4e33d164f0dccd95cca
jesus <jesusmanuel.maciasmartinez@gmail.com>
2022-12-15 21:38:16 -0500

    Export to CSV

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -9,12 +9,12 @@ if __name__ == "__main__":
     uid = int(argv[1])
     todos = requests.get("https://jsonplaceholder.typicode.com/users/{}/todos"
                          .format(uid))
-    usr_inf = requests.get("https://jsonplaceholder.typicode.com/users/{}"
+    usr_inf = requests.get("https://jsonplaceholder.typicode.com/users?id={}"
                            .format(uid))
 
     task_completed = 0
     task_total = 0
-    user_name = usr_inf.json()["name"]
+    user_name = usr_inf.json()[0]["username"]
 
     for task in todos.json():
         if task["completed"] is True:
@@ -24,12 +24,13 @@ if __name__ == "__main__":
     task_compl_list = []
 
     for task in todos.json():
-        csvcolums = [uid, user_name, str(task["completed"]), str(task["title"])]
+        csvcolums = [uid, user_name, str(task["completed"]),
+                     str(task["title"])]
         task_compl_list.append(csvcolums)
 
     csvheaders = ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"]
 
     with open("{}.csv".format(uid), 'w') as f:
         CsvWri = csv.writer(f, quoting=csv.QUOTE_ALL)
-        #CsvWri.writerow(csvheaders)
+        # CsvWri.writerow(csvheaders)
         CsvWri.writerows(task_compl_list)
